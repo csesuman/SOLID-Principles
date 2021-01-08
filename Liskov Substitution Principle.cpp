c6b2d58450b02908f8e45a56869859a13af54cde
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-01-09 03:43:28 +0600

    Add More Code

--- Liskov Substitution Principle.cpp
+++ Liskov Substitution Principle.cpp
@@ -45,8 +45,88 @@ class Ostrich : Bird { // Ostrich can not fly
 
 };
 
+// Again Rule Violation Example
+
+ // Violation of Likov's Substitution Principle
+class Rectangle {
+
+protected:
+    int width;
+    int height;
+
+public:
+    void setWidth(int width) {
+        this->width = width;
+    }
+
+    void setHeight(int height) {
+        this->height = height;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    int getArea() {
+        return width * height;
+    }
+};
+
+class Square : public Rectangle {
+
+public:
+    void setWidth(int width) {
+        this->width = width;
+        this->height = width;
+    }
+
+    void setHeight(int height) {
+        this->width = height;
+        this->height = height;
+    }
+
+};
+
+class LspTest {
+
+public:
+    static Rectangle getNewRectangle() {
+        // it can be an object returned by some factory ...
+        return Square();
+    }
+
+//    static void main() {
+//        Rectangle r = LspTest.getNewRectangle();
+//
+//        r.setWidth(5);
+//        r.setHeight(10);
+//        // user knows that r it's a rectangle.
+//        // It assumes that he's able to set the width and height as for the base
+//        // class
+//
+////        System.out.println(r.getArea());
+//        // now he's surprised to see that the area is 100 instead of 50.
+//    }
+};
+
+
 
 int main(){
+
+    Rectangle rec = LspTest::getNewRectangle();
+
+    rec.setHeight(5);
+    rec.setWidth(10);
+
+
+    cout << "Area is " << rec.getArea() << endl;
+
+    // this is wrong output
+
 
 
 
